a8d5d3e60300bf88e7f9f4373a2ac1235faba8bf
voxel!() <voxelmc@hotmail.com>
2024-01-20 11:47:24 -0800

    Add Vercel analytics and fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,39 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import smartypants from 'remark-smartypants';
 import remarkToc from 'remark-toc';
 import emoji from 'remark-emoji';
-
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic';
-
 import expressiveCode from 'astro-expressive-code';
 import metaTags from 'astro-meta-tags';
 // import tailwindConfigViewer from 'astro-tailwind-config-viewer';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
+	output: 'hybrid',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		}
+	}),
 	site: 'https://blog.trevfox.dev',
 	markdown: {
 		smartypants: false,
-		remarkPlugins: [[smartypants, { dashes: 'oldschool' }], remarkToc, emoji],
+		remarkPlugins: [
+			[
+				smartypants,
+				{
+					dashes: 'oldschool',
+				},
+			],
+			remarkToc,
+			emoji,
+		],
 		rehypePlugins: [
 			rehypeHeadingIds,
 			[
@@ -27,7 +41,6 @@ export default defineConfig({
 				{
 					behavior: 'append',
 					content: fromHtmlIsomorphic(
-
 						'<span class="ri-link-m ri-sm anchor-link" style="margin-left: 0.4rem;"></span>'
 					),
 				},
